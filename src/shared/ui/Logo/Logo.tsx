ebc5eaa947a06d37cf018c6b54a27fe28c36001f
DenisPolyakov1328 <122994941+DenisPolyakov1328@users.noreply.github.com>
2025-08-10 15:33:42 +0300

    Пофиксил предупреждение, обновив компонент Logo. Атрибут priority нужно передавать только главному логотипу

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,33 +1,39 @@
-import { Box, SxProps, Theme } from '@mui/material'
+import Link from 'next/link'
 import Image from 'next/image'
-import { ElementType } from 'react'
+import { Box, SxProps, Theme } from '@mui/material'
 
 interface LogoProps {
   href?: string
-  component?: ElementType
   sx?: SxProps<Theme>
   src?: string
+  priority?: boolean
 }
 
 export const Logo = ({
-  href,
-  component = 'div',
+  href = '/',
   sx = {},
-  src = '/KAPPEL_MARC_BLACK.svg'
+  src = '/KAPPEL_MARC_BLACK.svg',
+  priority = false
 }: LogoProps) => {
   return (
-    <Box
-      href={href}
-      component={component}
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        cursor: 'pointer',
-        ...sx
-      }}
-    >
-      <Image src={src} alt="Логотип" width={105} height={63} priority />
-    </Box>
+    <Link href={href} style={{ display: 'inline-flex' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          cursor: 'pointer',
+          ...sx
+        }}
+      >
+        <Image
+          src={src}
+          alt="Логотип"
+          width={105}
+          height={63}
+          priority={priority}
+        />
+      </Box>
+    </Link>
   )
 }
